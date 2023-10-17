18fc7805d547b304f5748bbe8f54fd323b840aa6
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-10-17 17:14:49 -0300

    add more quetions in insert 1, list dont work yet

--- main.asm
+++ main.asm
@@ -3,58 +3,132 @@ prompt:       .asciiz "Welcome to AppointmentAgenda\n"
 menu:         .asciiz "Select an option for your appointments\n1. Insert\n2. Remove\n3. Edit\n4. List\n0. Exit\n"
 input_prompt: .asciiz "Enter your choice: "
 event_prompt: .asciiz "Enter the name of the event: "
-events:       .space 1000 # Espaço para armazenar os eventos
+day_prompt:   .asciiz "Enter the day of the event: "
+start_prompt: .asciiz "Enter the starting time of the event (e.g., 13.30): "
+end_prompt:   .asciiz "Enter the ending time of the event (e.g., 14.30): "
+
+#             Defina a estrutura do nó do evento
+event_node:   .space 64
+
+#             Defina a estrutura de eventos
+event_list:   .word 0 # Início da lista de eventos
+event_count:  .word 0 # Contador de eventos
 
 .text
 .globl        main
 
 main:
-    #  exibe mensagem de boas-vindas
-    li $v0, 4            # código de impressão de string
-    la $a0, prompt       # endereço da mensagem de boas vindas
+    #  Exibe mensagem de boas-vindas
+    li $v0, 4            # Código de impressão de string
+    la $a0, prompt       # Endereço da mensagem de boas vindas
     syscall
 
 menu_loop:
-    #  exibe o menu
-    li $v0, 4            # código de impressão da string
-    la $a0, menu         # endereço do menu
+    #  Exibe o menu
+    li $v0, 4            # Código de impressão da string
+    la $a0, menu         # Endereço do menu
     syscall
 
-    #  solicita a escolha do usuário
-    li $v0, 4            # código de impressão da string
-    la $a0, input_prompt # endereço da mensagem de entrada
+    #  Solicita a escolha do usuário
+    li $v0, 4            # Código de impressão da string
+    la $a0, input_prompt # Endereço da mensagem de entrada
     syscall
 
-    #    le a escolha do usuário
-    li   $v0, 5            # código da leitura do inteiro
+    #    Lê a escolha do usuário
+    li   $v0, 5            # Código da leitura do inteiro
     syscall
-    move $t0, $v0        # armazena a escolha em $t0
+    move $t0, $v0        # Armazena a escolha em $t0
 
-    # veridica a escolha do usuário
-    beq $t0, $zero, exit  # Sair
-    beq $t0, 1, insert_event  # Inserir um evento
-	
-    # implementem as outras opções aqui
+    # Verifica a escolha do usuário
+    beq $t0, $zero, exit   # Sair
+    beq $t0, 1, insert_event # Inserir um evento
+    # Implemente outras opções aqui
 
     # Caso contrário, volta para o início do menu
     j menu_loop
 
 insert_event:
-    #  solicita o nome do evento
-    li $v0, 4              # codigo de impressao da string
-    la $a0, event_prompt   # endereco da mensagem do evento
+    #  Solicita o nome do evento
+    li $v0, 4              # Código de impressão de string
+    la $a0, event_prompt   # Endereço da mensagem do evento
     syscall
 
-    #  le o nome do evento
-    li $v0, 8              # codigo de leitura da string
-    la $a0, events         # edereco do buffer pra armazenar o evento
-    li $a1, 1000           # tamanho maximo do evento
+    #  Lê o nome do evento
+    li $v0, 8              # Código de leitura da string
+    la $a0, event_node     # Endereço do buffer para armazenar o evento
+    li $a1, 64             # Tamanho máximo do evento
     syscall
+
+    #  Solicita o dia do evento
+    li $v0, 4              # Código de impressão de string
+    la $a0, day_prompt     # Endereço da mensagem do dia
+    syscall
+
+    #    Lê o dia do evento
+    li   $v0, 5              # Código da leitura do inteiro
+    syscall
+    move $t1, $v0          # Armazena o dia em $t1
+
+    #  Solicita o horário de início do evento
+    li $v0, 4              # Código de impressão de string
+    la $a0, start_prompt   # Endereço da mensagem do horário de início
+    syscall
+
+    #     Lê o horário de início do evento
+    li    $v0, 6              # Código da leitura do número de ponto flutuante
+    syscall
+    mov.s $f0, $f0         # Move o valor de ponto flutuante para $f0
+
+    #  Solicita o horário de término do evento
+    li $v0, 4              # Código de impressão de string
+    la $a0, end_prompt     # Endereço da mensagem do horário de término
+    syscall
+
+    #     Lê o horário de término do evento
+    li    $v0, 6              # Código da leitura do número de ponto flutuante
+    syscall
+    mov.s $f1, $f0         # Move o valor de ponto flutuante para $f1
+
+    #   Chama a função para adicionar o evento à lista
+    jal add_event_to_list
 
     # Volta ao menu
     j menu_loop
 
+add_event_to_list:
+    #    Verifica se a lista de eventos está vazia
+    lw   $t2, event_list
+    beqz $t2, create_new_event_list
+
+    #    Se não estiver vazia, encontra o último evento na lista
+    move $t3,                  $t2
+    lw   $t2,                  ($t2)   # Próximo evento na lista
+    loop_find_last_event:
+        beqz $t2, add_to_existing_event_list
+        move $t3, $t2
+        lw   $t2, ($t2)  # Próximo evento na lista
+        j    loop_find_last_event
+
+    # Cria um novo evento na lista
+    create_new_event_list:
+        la $t2,   event_node  # Endereço do novo evento
+        sw $zero, ($t2)     # Inicializa o campo "prox" com zero
+        sw $t2,   event_list  # Define o novo evento como início da lista
+        addi $t0, $t0, 1   # Incrementa o contador de eventos
+        sw $t0,   event_count
+
+    # Adiciona os dados do evento no novo nó
+    add_to_existing_event_list:
+        sw    $t1, 4($t2)  # Armazena o dia do evento no nó
+        mov.s $f2, $f0  # Copia o horário de início para $f2
+        mov.s $f3, $f1  # Copia o horário de término para $f3
+        swc1  $f2, 8($t2)  # Armazena o horário de início no nó
+        swc1  $f3, 12($t2) # Armazena o horário de término no nó
+        move  $t3, $t2     # Atualiza o último nó na lista
+
+    jr $ra  # Retorna
+
 exit:
-    #  finaliza o programa
-    li $v0, 10  # código do serviço de saída
+    #  Finaliza o programa
+    li $v0, 10  # Código do serviço de saída
     syscall
