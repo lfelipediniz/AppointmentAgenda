902421b8d82e47a92107ef3c66ac2c8ba54c1133
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-10-20 15:17:16 -0300

    insert all values complete

--- main.asm
+++ main.asm
@@ -1,10 +1,9 @@
-
 #this script is a simple agenda for appointments make in MIPS assembly
 
 # data segment
 .data
-    #action inserted by the user
-    action: .word 0
+   #action inserted by the user
+   action: .word 0
 
    # counter of events
    eventCounter: .word 1
@@ -15,14 +14,26 @@
    # events name array with 1500 bytes
    eventsName: .space 1500
 
-   # events day array with 120 bytes, 30 days * 4 bytes
+   # events day array  with 120 bytes, 30 days * 4 bytes
    eventsDay: .space 120  
 
-    # strings returned to the user
-    actions: .asciiz "Available actions:\n[1] insert\n[2] print\n[3] remove\n[4] edit\n[0] quit\n"
-    invalidAction: .asciiz "Invalid Action!\n"
-    errorInput: .asciiz "Unable to insert :(\n"
-    welcome: .asciiz "Welcome to AppointmentAgenda!\n"
+   # events start time hours array with 120 bytes, 30 'hours' * 4 bytes
+   eventsStartTimeHours: .space 120
+
+   # events start time minutes array with 120 bytes, 30 'minutes' * 4 bytes
+   eventsStartTimeMinutes: .space 120
+
+   # events end time hours array with 120 bytes, 30 'hours' * 4 bytes
+   eventsEndTimeHours: .space 120
+
+   # events end time minutes array with 120 bytes, 30 'minutes' * 4 bytes
+   eventsEndTimeMinutes: .space 120
+
+   # strings returned to the user
+   actions: .asciiz "Available actions:\n[1] insert\n[2] print\n[3] remove\n[4] edit\n[0] quit\n"
+   invalidAction: .asciiz "Invalid Action!\n"
+   errorInput: .asciiz "Unable to insert :(\n"
+   welcome: .asciiz "Welcome to AppointmentAgenda!\n"
 
    # line break
    lineBreak: .asciiz "\n"
@@ -30,12 +41,16 @@
    # insert function outputs
    insert_eventName: .asciiz "What is the event name?\n"
    insert_eventDay: .asciiz "What day does this event occur?\n"
-   insert_eventStartTime: .asciiz "What time does this event start?\n"
-   insert_eventEndTime: .asciiz "What time does this event end?\n"
+   insert_eventStartTimeHours: .asciiz "What is the event start time (hours) |Format 24h|?\n"
+   insert_eventStartTimeMinutes: .asciiz "What is the event start time (minutes)?\n"
 
    # print function outputs
    print_eventName: .asciiz "Event name: "
    print_eventDay: .asciiz "Event day: "
+   print_eventStartTime: .asciiz "Event start time:\n"
+   print_eventStartTimeHours: .asciiz "Hour: "
+   print_eventStartTimeMinutes: .asciiz "Minute: "
+   print_eventEndTime: .asciiz "Event end time:\n"
 
 .text
 .globl main
@@ -60,13 +75,11 @@ insert:
       mul $t0, $t0, 50
 
    # reading the event name
-
       li $v0, 8
       la $a0, eventsName
       add $a0, $a0, $t0
       li $a1, 50
       syscall
-
 
    # eventCounter starts in 1, so we need to multiply it by 4 to get the correct position in the array
       lw $t0, eventCounter
@@ -78,18 +91,66 @@ insert:
       syscall
 
    # reading the event day
-         li $v0, 8
-         la $a0, eventsDay
-         add $a0, $a0, $t0
-         li $a1, 4
-         syscall
+      li $v0, 8
+      la $a0, eventsDay
+      add $a0, $a0, $t0
+      li $a1, 4
+      syscall
+
+   #printing the event start time hours question
+      li $v0, 4
+      la $a0, insert_eventStartTimeHours
+      syscall
+
+   # reading the event start time hours
+      li $v0, 8
+      la $a0, eventsStartTimeHours
+      add $a0, $a0, $t0
+      li $a1, 4
+      syscall
+
+   #printing the event start time minutes question
+      li $v0, 4
+      la $a0, insert_eventStartTimeMinutes
+      syscall
+
+   # reading the event start time minutes
+      li $v0, 8
+      la $a0, eventsStartTimeMinutes
+      add $a0, $a0, $t0
+      li $a1, 4
+      syscall
+
+   #printing the event end time hours question
+      li $v0, 4
+      la $a0, insert_eventStartTimeHours
+      syscall
+
+   # reading the event end time hours
+      li $v0, 8
+      la $a0, eventsEndTimeHours
+      add $a0, $a0, $t0
+      li $a1, 4
+      syscall
+
+   #printing the event end time minutes question
+      li $v0, 4
+      la $a0, insert_eventStartTimeMinutes
+      syscall
+
+   # reading the event end time minutes
+      li $v0, 8
+      la $a0, eventsEndTimeMinutes
+      add $a0, $a0, $t0
+      li $a1, 4
+      syscall
 
    # incresse at one the eventCounter
       lw $t0, eventCounter
       addi $t0, $t0, 1
       sw $t0, eventCounter
 
-   j loop_action
+      j loop_action
 
 # function of print all events
 print:
@@ -131,7 +192,77 @@ print:
       mul $t0, $t0, 4
       add $a0, $a0, $t0
       syscall
-         
+
+      # reset auxCounter to 1
+      lw $t0, auxCounter
+
+      # print text of event start time
+      li $v0, 4
+      la $a0, print_eventStartTime
+      syscall
+
+      # print text of event start time hours
+      li $v0, 4
+      la $a0, print_eventStartTimeHours
+      syscall
+
+      # print event start time hours
+      li $v0, 4
+      la $a0, eventsStartTimeHours
+      mul $t0, $t0, 4
+      add $a0, $a0, $t0
+      syscall
+
+      # reset auxCounter to 1
+      lw $t0, auxCounter
+
+      # print text of event start time minutes
+      li $v0, 4
+      la $a0, print_eventStartTimeMinutes
+      syscall
+
+      # print event start time minutes
+      li $v0, 4
+      la $a0, eventsStartTimeMinutes
+      mul $t0, $t0, 4
+      add $a0, $a0, $t0
+      syscall
+
+      # reset auxCounter to 1
+      lw $t0, auxCounter
+
+      # print text of event end time
+      li $v0, 4
+      la $a0, print_eventEndTime
+      syscall
+
+      # print text of event end time hours
+      li $v0, 4
+      la $a0, print_eventStartTimeHours
+      syscall
+
+      # print event end time hours
+      li $v0, 4
+      la $a0, eventsEndTimeHours
+      mul $t0, $t0, 4
+      add $a0, $a0, $t0
+      syscall
+
+      # reset auxCounter to 1
+      lw $t0, auxCounter
+
+      # print text of event end time minutes
+      li $v0, 4
+      la $a0, print_eventStartTimeMinutes
+      syscall
+
+      # print event end time minutes
+      li $v0, 4
+      la $a0, eventsEndTimeMinutes
+      mul $t0, $t0, 4
+      add $a0, $a0, $t0
+      syscall
+
       # increment auxCounter
       lw $t0, auxCounter
       addi $t0, $t0, 1
@@ -142,54 +273,53 @@ print:
       la $a0, lineBreak
       syscall
 
-      
-      j loop_print
+      j loop_print 
 
     exit_print:
         j loop_action
 
-# função para remover um evento
+# function to remove an event
 remove:
     j loop_action
 
-# função para editar um evento que já existe
+# function to edit an event
 edit:
     j loop_action
 
 main:
-    # loop que pergunta as acoes desejadas, até que o usuário queira sair
+    # loop quest the action to the user
     loop_action:
-        # impressão da pergunta
+        # print the available actions
         li $v0, 4
         la $a0, actions
         syscall
 
-        # leitura do número inteiro que representa a ação
+        # read the int number of the action
         li $v0, 5
         syscall
 
-        # armazenando o valor lido em action
+        # storaging the action in the action variable
         sw $v0, action
 
-        # verificando se action é igual a 0 (quit)
+        # verify if the action is 0, if yes, quit the program
         lw $t0, action
         beq $t0, $zero, quit
 
-        # se a ação for 1, 2, 3 ou 4, chama a função correspondente
+        # call the function according to the action
         beq $t0, 1, insert
         beq $t0, 2, print
         beq $t0, 3, remove
         beq $t0, 4, edit
 
-        # caso a ação não seja válida
+        # case the action is invalid, print the invalidAction message
         li $v0, 4
         la $a0, invalidAction
         syscall
 
-        # chamamos o loop aqui caso o usuário digite uma ação ilegal
+        # for ilegal actions, we need to call the loop_action again
         j loop_action
 
 quit:
-    # encerrando o programa
+    # exit the program
     li $v0, 10
     syscall
