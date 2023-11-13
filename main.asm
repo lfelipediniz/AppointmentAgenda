312631ebbe0ba38085584ee711b0bd1d4e2c6157
lfelipediniz <lfediniz@gmail.com>
2023-11-13 17:32:13 -0300

    test

--- main.asm
+++ main.asm
@@ -9,7 +9,7 @@
       MAX_LENGTH_EVENT_NAME: .word 50
       MIN_HOUR: .float 0.0
       MAX_HOUR: .float 23.59
-      MAX_MINUTES: .float 0.59
+      MAX_MINUTES: .float 0.599
       ONE: .float 1.0
       NEGATIVE_ONE: .float -1.0
 
@@ -138,6 +138,7 @@ insert:
       # for minutes greater than 59, we need to print the errorInput message
          # aux_startTime to f0
          l.s $f0, aux_eventStartTime
+         l.s $f1, MAX_MINUTES
 
          # Convert float to integer (truncate)
          trunc.w.s $f4, $f0 
@@ -155,23 +156,11 @@ insert:
          # print the value of f0 (debug)
          li $v0, 2            
          mov.s $f12, $f0      
-         syscall               
+         syscall
 
-
-
-
-
-
-
-      
-
-
-
-
-
-
-
-
+         # compare if the value of f0 is greater than 0.59
+         c.lt.s $f1, $f0  
+         bc1t errorInsert  
 
    # print event end time
       li $v0, 4
@@ -199,6 +188,32 @@ insert:
       l.s $f12, aux_eventStartTime
       c.lt.s $f0, $f12 # Compare if $f0 less than $f12
       bc1t errorInsert  # if true, print error message
+
+      # for minutes greater than 59, we need to print the errorInput message
+         # aux_startTime to f0
+         l.s $f20, aux_eventEndTime
+
+         # Convert float to integer (truncate)
+         trunc.w.s $f14, $f20 
+
+         # move the integer part to an integer register $t0
+         mfc1 $t9, $f14
+
+         # Convert integer to float
+         mtc1 $t9, $f22    # move the integer part to a float register $f15
+         cvt.s.w $f22, $f22 # convert the integer part to a float
+
+         # subtract the integer value of the original value, f0 = f0 - f15
+         sub.s $f20, $f20, $f22
+
+         # print the value of f0 (debug)
+         li $v0, 2            
+         mov.s $f12, $f20      
+         syscall
+
+         # compare if the value of f0 is greater than 0.59
+         c.lt.s $f1, $f20  
+         bc1t errorInsert  
 
       j compareDay
       
